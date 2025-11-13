e1c8478cacf2ead0caa635a40e7cf4e30076ef13
Gandhi89 <shivamgandhi95@gmail.com>
2025-11-13 14:24:33 -0500

    fix: remove lock

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,6 @@
 import { Stack } from "expo-router";
-import * as ScreenOrientation from "expo-screen-orientation";
-import { useEffect } from "react";
 
 export default function RootLayout() {
-  useEffect(() => {
-    // Lock screen orientation to landscape
-    ScreenOrientation.lockAsync(
-      ScreenOrientation.OrientationLock.LANDSCAPE
-    );
-    
-    // Cleanup: unlock on unmount (optional)
-    return () => {
-      ScreenOrientation.unlockAsync();
-    };
-  }, []);
 
   return <Stack screenOptions={{ headerShown: false }} />;
 }
